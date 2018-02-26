4d1b5e51ff5850bd7c06e2e3c659ef1345a6c195
fionajovita <35193761+fionajovita@users.noreply.github.com>
2018-02-26 19:23:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile('Article one is requested and will be served here!');
+});
+
+app.get('/article-one', function (req, res) {
+  res.sendFile('Article two is requested and will be served here!');
+});
+
+app.get('/article-one', function (req, res) {
+  res.sendFile('Article three is requested and will be served here!');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
