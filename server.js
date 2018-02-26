94c72bb9f1fc4ab4d2efa9050f89e703a8864c15
fionajovita <35193761+fionajovita@users.noreply.github.com>
2018-02-26 21:13:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,16 +82,11 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     //articleName==article-one
+    var articleName= req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
